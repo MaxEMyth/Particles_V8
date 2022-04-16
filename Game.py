a88694ae21b992fc369a382e5b5f318f302663b0
MaxEMyth <maxmele@hotmail.com>
2022-04-16 17:52:09 -0300

    Fiddling with zooming mechanism (Removed scaling lag issue)

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from pygame import *
+from pygame.gfxdraw import textured_polygon
 from pygame import locals as l
 
 from Bindings import binds
@@ -24,10 +25,9 @@ class Game:
         self.settings = Setup()
         self.cam = self.Camera()
         self.clock = time.Clock()
-        self.screen = display.set_mode(self.settings.GAME_FIELD_SIZE, l.RESIZABLE)
-        # self.background =image.load("BG.png")
+        self.screen = display.set_mode(self.settings.VIEWPORT_SIZE, l.RESIZABLE )
         self.background = image.load("Particles_V8\BG.png")
-        self.scaled_background = self.background  #to be scaled later 
+        self.scaled_background = self.background.copy()  # to be scaled later 
         self.running = True
     
     def handle_all_events(self) -> int:
@@ -46,10 +46,12 @@ class Game:
                     match recieved_event.y:  
                         # recieved_event.y value is 1 for wheelup, -1 for wheeldown
                         case binds.mouse.wheelup if self.running:
-                            self.cam.inflate_ip(-10, -10)
+                            # self.cam.inflate_ip(Vector2(-100, -100)/self.cam.relative_size_fraction)
+                            self.cam.inflate_ip(Vector2(-10, -10))
                             print("Zoom in")
                         case binds.mouse.wheeldown if self.running:
-                            self.cam.inflate_ip(10, 10)
+                            # self.cam.inflate_ip(Vector2(100, 100)/self.cam.relative_size_fraction)
+                            self.cam.inflate_ip(Vector2(10, 10))
                             print("Zoom out")
                 case l.KEYDOWN:
                     # Update keystate:
@@ -88,6 +90,7 @@ class Game:
                 # case _:
                     print("Uncaught Event: ", recieved_event)  # event.event_name(event.type))
         return (time.get_ticks() - start_time)
+    
     def handle_held_keys(self) -> None:  # for continuous actions, checked every frame.
         self.cam.vel = self.cam.max_vel * math.Vector2(
                 self.keyboard.state[binds.right] - self.keyboard.state[binds.left],
@@ -100,18 +103,20 @@ class Game:
         Returns:
             int: Elapsed background transformation time in milliseconds (ms).
         """
-        self.scaled_background = transform.scale(self.background, (self.settings.VIEWPORT_SIZE//1)*self.cam.relative_size_fraction)
-        self.screen.blit(self.scaled_background, ((0, 0) - Vector2(self.cam.topleft)) * self.cam.relative_size_fraction,)
-
+        start_time = time.get_ticks()
+        game_field_rect = Rect((0,0), self.settings.GAME_FIELD_SIZE)
+        if Vector2.magnitude(Vector2(self.cam.size)) <= Vector2.magnitude(self.settings.VIEWPORT_SIZE):
+            
+            cropped_background = self.background.subsurface(Rect(self.cam).clip(game_field_rect))
+            transform.scale(cropped_background, self.settings.VIEWPORT_SIZE, self.screen)
+            
+        return (time.get_ticks() - start_time)
     
     class Camera(Rect):
         def __init__(self) -> None:
             Rect.__init__(self, (0, 0) , (500, 500))
             print("Initializing Camera")
-            self.vel =math.Vector2(0, 0)
-            # self.topleft,  , 
-            # self.size = 
-            # self.center = self.topleft + self.size * 0.5
+            self.vel =Vector2(0, 0)
             self.max_vel = 150
             self.zoom_factor = 1.1
             self.relative_size_fraction = 1.
@@ -120,8 +125,10 @@ class Game:
             self.clamp_ip(Rect(
                 -self.width,
                 -self.height,
-                game.settings.VIEWPORT_SIZE.x + 2*self.width,
-                game.settings.VIEWPORT_SIZE.y + 2*self.height
+                display.get_window_size()[0] + 2*self.width,
+                display.get_window_size()[1] + 2*self.height
+                # game.settings.VIEWPORT_SIZE.x + 2*self.width,
+                # game.settings.VIEWPORT_SIZE.y + 2*self.height
                 ))
         def update(self) -> None:
             # self.center = Vector2(self.topleft) + Vector2(self.size) * 0.5
