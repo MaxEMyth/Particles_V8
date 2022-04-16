5dc123e5207ff1247fe90c8487a0e54fecdcf0ac
MaxEMyth <maxmele@hotmail.com>
2022-04-16 14:16:46 -0300

    Switched Viewport and Game_field size.

--- Setup.py
+++ Setup.py
@@ -3,9 +3,9 @@ from pygame import Vector2
 
 class Setup:
     def __init__(self) -> None:
-        self.VIEWPORT_SIZE = Vector2(1000, 1000)  # ! These values should be flipped!
-        self.GAME_FIELD_SIZE = Vector2(500, 500)  # !
-        self.FPS = 1
+        self.VIEWPORT_SIZE = Vector2(500, 500)
+        self.GAME_FIELD_SIZE = Vector2(1000, 1000)
+        self.FPS = 60
         self.N = 12
         self.COZINESS_FACTOR = 0.2  # max should be ~0.9
         self.WALL_FACTOR = 1  # .987,                # 0 - 0.985
