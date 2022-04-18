c9c1976465cdb0592166f35ee4729cf7c5329b26
MaxEMyth <maxmele@hotmail.com>
2022-04-18 01:19:11 -0300

    Changed definitions to use pygame Color class.

--- Colors.py
+++ Colors.py
@@ -1,5 +1,8 @@
-BLACK = (0, 0, 0)
-GREEN = (50, 180, 50)
-RED = (240, 20, 20)
-BLUE = (50, 50, 255)
-WHITE = (255, 255, 255)
+from pygame import Color
+
+
+BLACK = Color(0, 0, 0, 110)
+GREEN = Color(50, 180, 50)
+RED   = Color(240, 20, 20)
+BLUE  = Color(50, 50, 255)
+WHITE = Color(255, 255, 255)
