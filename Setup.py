b0acd2084abc721e2ffa8a81584bc08be28eed3d
MaxEMyth <maxmele@hotmail.com>
2022-04-18 01:23:11 -0300

    Removed old, "deprecated" settings.

--- Setup.py
+++ Setup.py
@@ -7,8 +7,7 @@ class Setup:
         self.GAME_FIELD_SIZE = Vector2(1000, 1000)
         self.FPS = 60
         self.N = 12
-        self.COZINESS_FACTOR = 0.2  # max should be ~0.9
         self.WALL_FACTOR = 1  # .987,                # 0 - 0.985
         self.GRAVITY = 0  # units - > px/s**2
-        self.TOTAL_MOMENTUM = 500
         self.precise = False
+        self.zoom_limit = Vector2(10,10)
